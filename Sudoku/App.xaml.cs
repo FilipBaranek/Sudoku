70f9c00ed1618078be894b2c2fa4021ad1ff39bc
FilipBaranek <filipbaranek22@gmail.com>
2025-04-28 21:19:21 +0200

    Installation files

--- Sudoku/App.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows;
-
-namespace Sudoku
-{
-    public partial class App : Application
-    {
-    }
-
-}
--- Sudoku/Models/Game/Difficulty.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sudoku.Models
-{
-    public enum Difficulty
-    {
-        Easy = 55,
-        Medium = 45,
-        Hard = 30
-    }
-}
